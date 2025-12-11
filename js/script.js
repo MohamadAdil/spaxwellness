6ff6b58d5edf34a0fafdd7588cc850066cf8aea5
Mohamad Adil <adilansari8727@gmail.com>
2025-12-11 18:31:37 +0530

    '11-12-2025'

--- js/script.js
+++ js/script.js
@@ -39,4 +39,37 @@ jQuery(function () {
   jQuery(window).on("scroll resize", startCounters);
   startCounters();
 });
+// 
+const swiper = new Swiper(".myreviews", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: false,
 
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+    1400: { slidesPerView: 4.5 }
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+// 
+const video = document.getElementById("my-video");
+const playButton = document.getElementById("play-button");
+const coverImage = document.getElementById("cover-image");
+
+playButton.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    coverImage.style.display = "none";  // Hide the cover image when video starts
+    playButton.style.display = "none";  // Hide play button when video starts
+  } else {
+    video.pause();
+    coverImage.style.display = "block";  // Show the cover image when video pauses
+    playButton.style.display = "block";  // Show play button when video pauses
+  }
+});
